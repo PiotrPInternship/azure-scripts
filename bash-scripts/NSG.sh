edf7fa9140935fcf7f2c26b82f823b467c68edff
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-22 11:18:11 +0200

    Create NSG.sh

---
+++ bash-scripts/NSG.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Creating NSG and rule"
+$nsgname="NSG1"
+$resourcegroup="script-test"
+$location1="westeurope"
+
+$rulename="my1st-rule"
+$rulepriority=1000
+az network nsg create --name $nsgname --resource-group $resourcegroup --location $location1
+az network nsg rule create --nsg-name $nsgname --resource-group $resourcegroup --name $rulename --priority $rulepriority
