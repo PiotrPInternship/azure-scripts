1c993f24f57b3c13c891fc3b4a634cb999de458f
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-23 10:21:38 +0200

    Update vnet.sh

--- bash-scripts/vnet.sh
+++ bash-scripts/vnet.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Creating VNET"
 
-$vnetname="vnet1"
-$resourcegroup="script-test"
-$location1="westeurope"
-$addressspace="10.1.0.0/16"
-# $subnetsArray="(Subnet1 10.1.0.0/24 Subnet2 10.1.1.0/24 Subnet3 10.1.2.0/24)"
-# $subnets=3
-$subnet1="Subnet1"
-$subnet1adress="10.1.0.0/24"
+vnetname="vnet1"
+resourcegroup="script-test"
+location1="westeurope"
+addressspace="10.1.0.0/16"
+# subnetsArray="(Subnet1 10.1.0.0/24 Subnet2 10.1.1.0/24 Subnet3 10.1.2.0/24)"
+# subnets=3
+subnet1="Subnet1"
+subnet1adress="10.1.0.0/24"
 
 az network vnet create --name $vnetname --resource-group $resourcegroup --location $location1 --address-prefix $addressspace --subnet-name $subnet1 --subnet-prefix $subnet1adress
 # az network vnet create --name $vnetname --resource-group $resourcegroup --location $location1 --address-prefix $addressspace --subnet-name ${subnetsArray[0]} --subnet-prefix ${subnetsArray[1]}
