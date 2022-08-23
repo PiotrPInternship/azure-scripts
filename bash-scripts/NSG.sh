b87ea6cd310387f606baa20f517b9e6b3bfb93c5
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-23 12:25:05 +0200

    Update NSG.sh

--- bash-scripts/NSG.sh
+++ bash-scripts/NSG.sh
@@ -6,5 +6,7 @@ location1="westeurope"
 
 rulename="my1st-rule"
 rulepriority=1000
+arrayofrules(Allow Outbound SSH)
+
 az network nsg create --name $nsgname --resource-group $resourcegroup --location $location1
-az network nsg rule create --nsg-name $nsgname --resource-group $resourcegroup --name $rulename --priority $rulepriority
+az network nsg rule create --nsg-name $nsgname --resource-group $resourcegroup --name $rulename --priority $rulepriority --acces ${arrayofrules[0]} --direction ${arrayofrules[1]} --protocool ${arrayofrules[2]}
