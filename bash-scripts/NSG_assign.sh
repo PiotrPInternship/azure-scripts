831a1f1323e375c967120792ee462e77139a046c
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-23 13:38:20 +0200

    Update NSG_assign.sh

--- bash-scripts/NSG_assign.sh
+++ bash-scripts/NSG_assign.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "Assigning NSG to VNet subnet"
+echo "Assigning NSG to VNet subnets"
 nsgname="NSG1"
 resourcegroup="script-test"
 vnetname="vnet1"
-subnet1="Subnet1"
+subnetsarray=(Subnet1 Subnet2)
+amountofsubnets=2
+for ((i=0; $((i<$amountofsubnets)); i++))
+do
+    az network vnet subnet update --resource-group $resourcegroup --name ${subnetsarray[$i]} --vnet-name $vnetname --network-security-group $nsgname
+done  
 
-az network vnet subnet update --resource-group $resourcegroup --name $subnet1 --vnet-name $vnetname --network-security-group $nsgname
