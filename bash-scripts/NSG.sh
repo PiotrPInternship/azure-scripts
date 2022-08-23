b932427f0ea6caaeadf964d6e370038ef2fa0a11
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-23 13:22:48 +0200

    Update NSG.sh

--- bash-scripts/NSG.sh
+++ bash-scripts/NSG.sh
@@ -5,8 +5,9 @@ resourcegroup="script-test"
 location1="westeurope"
 
 rulename="my1st-rule"
+arrayofrules=(Allow Outbound Tcp)
 rulepriority=1000
-arrayofrules(Allow Outbound SSH)
+
 
 az network nsg create --name $nsgname --resource-group $resourcegroup --location $location1
-az network nsg rule create --nsg-name $nsgname --resource-group $resourcegroup --name $rulename --priority $rulepriority --acces ${arrayofrules[0]} --direction ${arrayofrules[1]} --protocool ${arrayofrules[2]}
+az network nsg rule create --nsg-name $nsgname --resource-group $resourcegroup --name $rulename --priority $rulepriority --acces ${arrayofrules[0]} --direction ${arrayofrules[1]} --protocol ${arrayofrules[2]}
