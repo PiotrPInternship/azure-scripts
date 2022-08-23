f709e2f346c24d3c416b36162d304136e63bc613
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-23 11:52:55 +0200

    Update vnet.ps1

--- powershell-scripts/vnet.ps1
+++ powershell-scripts/vnet.ps1
@@ -1,1 +1,44 @@
+# #creating Vnet
+# Write-Output "creating Vnet"
+# $vnet = @{
+#     Name = 'vnet1'
+#     ResourceGroupName = 'script-test'
+#     Location = 'westeurope'
+#     AddressPrefix = '10.1.0.0/16'    
+# }
+# $vnetcreate = New-AzVirtualNetwork @vnet
 
+# $subnet = @{
+#     Name = 'subnet1'
+#     AddressPrefix = '10.1.0.0/24'
+# }
+# $subnetcreate = New-AzVirtualNetworkSubnetConfig @subnet
+# $subnetupdate = Add-AzVirtualNetworkSubnetConfig @subnet -VirtualNetwork $vnet #someghow it doesnt work
+
+#creating Vnet
+Write-Output "creating Vnet"
+$vnet = @{
+    Name = 'vnet1'
+    ResourceGroupName = 'script-test'
+    Location = 'westeurope'
+    AddressPrefix = '10.1.0.0/16'    
+}
+$virtualNetwork = New-AzVirtualNetwork @vnet
+# Subnets
+$subnets = 
+@'
+SubnetName,AddressPrefix
+"subnet1","10.1.0.0/24" 
+"subnet2","10.1.1.0/24"
+"subnet3","10.1.2.0/24"
+"subnet4","10.1.3.0/24"
+'@ | 
+ConvertFrom-Csv
+
+
+foreach ($subnet in $subnets) {
+ 
+    $dmz = New-AzVirtualNetworkSubnetConfig -Name $subnet.SubnetName -AddressPrefix $subnet.AddressPrefix
+    $updatedvnet=Add-AzVirtualNetworkSubnetConfig -Name $dmz.Name -VirtualNetwork $virtualNetwork -AddressPrefix $dmz.AddressPrefix
+    $updatedvnet | Set-AzVirtualNetwork
+}
