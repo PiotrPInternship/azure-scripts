f3392a4bd9d24a964a3be2c6eac78ae2d18cf775
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-08-25 08:18:35 +0200

    Create appservice.sh

---
+++ bash-scripts/appservice.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+#resource group create
+
+resourcegroup ='storage-resource-group'
+location1='westeurope'
+# New-AzResourceGroup -Name $resourcegroup -Location $location1
+
+#app service Linux based creating
+
+serviceplan='linux-app-service-plan'
+workers=3
+pricingtier='B1'
+az appservice plan create -n $serviceplan -g 'storage-resource-group' --location $location1 --is-linux --number-of-workers $workers --sku $pricingtier 
+
+appname='LinuxDockerApp-ppaszek'
+
+az webapp create --resource-group 'storage-resource-group' --plan $serviceplan --name $appname --deployment-container-image-name mcr.microsoft.com/dotnet/samples:aspnetapp
